c41187e28310674982cdffcc12f9a3080d6bf203
Ragdata <ragdata@users.noreply.github.com>
2022-07-24 06:30:18 +1000

    Initial Commit

---
+++ lib/jekyll/liquid/data.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "data/version"
+
+module Jekyll
+  module Liquid
+    module Data
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
